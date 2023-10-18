1be8ee255f1c3f8d44cd121c686329f6f8d168e9
Maxence <maxxence.marechal@gmail.com>
2023-10-18 14:20:37 +0200

    Add function to load more movies with button + enhancing code on moviePage

--- src/components/Home.jsx/Home.jsx
+++ src/components/Home.jsx/Home.jsx
@@ -1,55 +1,57 @@
 import React, { useEffect, useState } from "react";
 import "./home.css";
 import cinemaMidjourneyWebp from "./images/cinemaMidjourneyWebp.webp";
+import axios from "axios";
 
 // Imports des composants
 import MovieCards from "../movieCards/MovieCards";
 import SliderLayout from "../sliderLayout/SliderLayout";
 
-import { Helmet } from "react-helmet"; // Pour gérer dynamiquement les titres dans les onglets
+// Pour gérer dynamiquement les titres dans les onglets
+import { Helmet } from "react-helmet"; 
 
-//Import Bibliotheque Slicker carousel
+//Import Bibliothèque Slicker carousel
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+/**
+ * The Home component fetches data from two different APIs and displays the top-rated movies and the latest horror movies in a slider layout.
+ */
 const Home = () => {
   const [topRatedMovies, setTopRatedMovies] = useState([]);
   const [horrorMovies, setHorrorMovies] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+   
+
     const fetchData = async () => {
       try {
         //Appel API pour les mieux notés
-        const response = await fetch("https://api.tvmaze.com/shows?q=rating");
-        const result = await response.json();
+        const response = await axios.get("https://api.tvmaze.com/shows?q=rating");
+        const result = response.data;
         if (Array.isArray(result) && result.length > 0) {
           setTopRatedMovies(result);
         }
 
         //Appel API pour les films d'horreur
-        const horrorResponse = await fetch("https://api.tvmaze.com/shows");
-        const horrorResult = await horrorResponse.json();
+        const horrorResponse = await axios.get("https://api.tvmaze.com/shows");
+        const horrorResult = horrorResponse.data;
         setHorrorMovies(horrorResult);
-        // console.log(horrorResult, "Avant le tri \"horror")
 
         const horrorMoviesFiltered = horrorResult.filter((el) =>
-        el.genres.includes("Horror")
-      );   
-      // console.log(horrorMoviesFiltered, "Apres le tri \"horror")
+          el.genres.includes("Horror")
+        );
 
-      horrorMoviesFiltered.sort((a, b) => {
-       
-        const dateA = new Date(a.premiered);
-        const dateB = new Date(b.premiered);
-        return dateB - dateA; // Tri par ordre décroissant par date de sortie
-      });
-
-
-      setHorrorMovies(horrorMoviesFiltered);
-        // console.log(horrorResult,'HorrorResult')
+        // Tri par ordre décroissant et date de sortie
+        horrorMoviesFiltered.sort((a, b) => {
+          const dateA = new Date(a.premiered);
+          const dateB = new Date(b.premiered);
+          return dateB - dateA;
+        });
+        setHorrorMovies(horrorMoviesFiltered);
       } catch (error) {
         setError(true);
         setErrorMessage(
@@ -65,15 +67,13 @@ const Home = () => {
     fetchData();
   }, []);
 
-  const movieFilter = [...topRatedMovies]; // Destructuring
-  
-  movieFilter.sort((a, b) => b.rating.average - a.rating.average); // Triage par ordre décroissant en fonction de la note
+  const topRating = [...topRatedMovies] // Destructuring
+    .sort((a, b) => b.rating.average - a.rating.average) // Triage par ordre décroissant en fonction de la note
+    .slice(0, 20); //Renvoie une copie du tableau d'origine, ici les 20 premiers
 
-  const topRating = movieFilter.slice(0, 20); // Renvoie une copie du tableau d'origine, ici les 20 premiers
-  
   return (
     <>
-      {error && <p style={{ color: "red" }}>{errorMessage}</p>}
+      {error && <p className="errorMessage">{errorMessage}</p>}
 
       <section className="container">
         <Helmet>
@@ -93,6 +93,7 @@ const Home = () => {
 
         <div className="slider_container">
           <h3>Les mieux notés</h3>
+
           <Slider {...SliderLayout}>
             {topRating.map((movie) => (
               <MovieCards key={movie.id} movie={movie} />
@@ -102,6 +103,7 @@ const Home = () => {
 
         <div className="slider_container">
           <h3>Les derniers films d'horreur</h3>
+
           <Slider {...SliderLayout}>
             {horrorMovies.map((movie) => (
               <MovieCards key={movie.id} movie={movie} />
--- src/pages/movie/MoviePage.jsx
+++ src/pages/movie/MoviePage.jsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from "react";
 import "./moviePage.css";
+
+//Import des composants
 import Header from "../../components/header/Header";
-import { Helmet } from "react-helmet"; // Pour gérer dynamiquement les titres dans les onglets
+import MovieCards from "../../components/movieCards/MovieCards";
 import Searchbar from "../../components/searchBar/Searchbar";
 import { instance } from "../../api/axiosInstance";
-// import axios from "axios";
-import MovieCards from "../../components/movieCards/MovieCards";
+
+import { Helmet } from "react-helmet"; // Pour gérer dynamiquement les titres dans les onglets
 
 export default function MoviePage() {
   const [textSearchInput, setSearchInput] = useState("");
   const [shows, setShows] = useState([]);
   const [listShows, setListShows] = useState([]);
-
   const [errorMessage, setErrorMessage] = useState("");
   const [error, setError] = useState(false);
-
-  // console.log(shows);
+  const [visibleMovies, setVisibleMovies] = useState(20); // Par défaut, affiche 20 films
 
   useEffect(() => {
     const getShows = async () => {
@@ -33,12 +33,11 @@ export default function MoviePage() {
         setListShows(movieListResult);
 
         movieListResult.sort((a, b) => {
-          
           const dateA = new Date(a.premiered);
           const dateB = new Date(b.premiered);
           return dateB - dateA; // Tri par ordre décroissant
         });
-      
+
         setListShows(movieListResult);
       } catch (error) {
         setError(true);
@@ -52,7 +51,11 @@ export default function MoviePage() {
     getShows();
   }, [textSearchInput]);
 
-const recentMovies = listShows.slice(0, 20);
+  const recentMovies = listShows.slice(0, visibleMovies);
+
+  const loadMoreMovies = () => {
+    setVisibleMovies((prevVisibleMovies) => prevVisibleMovies + 20);
+  };
 
   return (
     <>
@@ -61,12 +64,11 @@ const recentMovies = listShows.slice(0, 20);
         textSearchInput={textSearchInput}
         setSearchInput={setSearchInput}
       />
-      {error && <p style={{ color: "red" }}>{errorMessage}</p>}
+      {error && <p className="errorMessage">{errorMessage}</p>}
       <div className="film_container">
         <Helmet>
           <title>Films</title>
         </Helmet>
-        {/* <h3 style={{ color: "white" }}> Resultats :</h3> */}
 
         <div className="shows">
           {shows.map((show) => {
@@ -74,19 +76,21 @@ const recentMovies = listShows.slice(0, 20);
           })}
         </div>
 
-        <div >
+        <div>
           <div>
             <h3>Les plus récents</h3>
           </div>
-          
-            <div className="mostRecentMovies">
-              {recentMovies.map((movie) => {
-                return <MovieCards key={movie.id} movie={movie} />;
-              })}
-            </div>
-         
+          <div className="mostRecentMovies">
+            {recentMovies.map((movie) => {
+              return <MovieCards key={movie.id} movie={movie} />;
+            })}
+          </div>
+          {visibleMovies < listShows.length && (
+            <button className="buttonLoadMore" onClick={loadMoreMovies}>
+              Recharger plus
+            </button>
+          )}
         </div>
-        
       </div>
     </>
   );
